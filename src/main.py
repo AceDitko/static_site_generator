b3c0c78bc6bb81ad8e777185a53d71c35bdfa189
AceDitko <jacob.mj.oliver@gmail.com>
2025-09-01 19:23:40 +0100

    add text_node_to_html_node function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from textnode import TextNode, TextType
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
+from functions import text_node_to_html_node
 
 print(TextNode("This is some anchor text", "link", "https://www.boot.dev"))
+
