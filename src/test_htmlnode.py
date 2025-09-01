c81a5c90f7e4c2f2001f17d08c3e44445c8e1735
AceDitko <jacob.mj.oliver@gmail.com>
2025-09-01 18:52:02 +0100

    add leafnode tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TextHTMLNode(unittest.TestCase):
     def test_noneProp(self):
@@ -15,3 +15,14 @@ class TextHTMLNode(unittest.TestCase):
         node = HTMLNode("p", "I love refrigerators", [], {"href": "https://www.google.com", "target": "_blank"})
         self.assertEqual(node.props_to_html(), ' href="https://www.google.com" target="_blank"')
    
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_h1(self):
+        node = LeafNode("h1", "Hello, world!")
+        self.assertEqual(node.to_html(), "<h1>Hello, world!</h1>")
+
+    def test_leaf_to_html_prop(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
