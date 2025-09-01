cf0438c18bcf92d713c456e7e197569f3d0cdfc1
AceDitko <jacob.mj.oliver@gmail.com>
2025-09-01 19:01:33 +0100

    add parent node tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,8 +1,8 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
-class TextHTMLNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_noneProp(self):
         node = HTMLNode("p", "I love refrigerators", [])
         self.assertEqual(node.props_to_html(), "")
@@ -26,3 +26,22 @@ class TextHTMLNode(unittest.TestCase):
     def test_leaf_to_html_prop(self):
         node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
         self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_children_props(self):
+        child_node = LeafNode("span", "child", {"href": "https://www.google.com"})
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), '<div><span href="https://www.google.com">child</span></div>')
