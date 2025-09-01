1c70572b6d9b7d93818f350a1f49a9eb19fa6a92
AceDitko <jacob.mj.oliver@gmail.com>
2025-09-01 17:49:05 +0100

    add htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props is None:
+            raise ValueError("Node has no attributes to convert to html")
+        out_string = ""
+        for key, value in self.props:
+            out_string += f" {key}={value}"
+        return out_string
+    
+    def __repr__(self):
+        return f"Tag={self.tag}\nValue={self.value}\nChildren={self.children}\nProps={self.props}"
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
 from textnode import *
+from htmlnode import *
 
 print(TextNode("This is some anchor text", "link", "https://www.boot.dev"))
